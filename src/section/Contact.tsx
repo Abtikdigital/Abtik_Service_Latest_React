493c40c3ab3ead4bd34ef0dd2c9e59811d823134
Abtik Digital <rbhensla1994@gmail.com>
2025-07-21 15:12:32 +0530

    Updated Code

--- src/section/Contact.tsx
+++ src/section/Contact.tsx
@@ -1,4 +1,4 @@
-// import India from "@react-map/india";
+import Map from "./Map.jsx"
 import Chart from "react-google-charts";
 const Contact = () => {
   const data = [
@@ -9,7 +9,14 @@ const Contact = () => {
   ];
 
   const options = {
-    title: "My Daily Activities",
+    title: "Business Segments",
+    titleTextStyle: {
+      fontName: "Anton", // <-- Your chosen font family
+      fontSize: 30, // <-- Desired font size in pixels
+      bold: true, // Optional: make the title bold
+      color: "#233238", // Optional: set the title color
+    },
+
     pieHole: 0.4, // Creates a Donut Chart. Does not do anything when is3D is enabled
     is3D: true, // Enables 3D view
     // slices: {
@@ -21,18 +28,20 @@ const Contact = () => {
     legend: {
       position: "bottom",
       alignment: "center",
+
       textStyle: {
         color: "#233238",
         fontSize: 14,
       },
     },
+
     colors: ["#3CA2E2", "#052EAA", "#2D87C4"],
   };
 
   return (
     <section className="px-14 py-6 bg-[#f7f7f7]">
       <div className="grid grid-cols-4 grid-rows-1 space-x-6">
-        <div className=" space-y-5 px-3">
+        <div className=" space-y-5 px-3 flex flex-col justify-center">
           <h2
             className="sub-heading to-[#052EAA] bg-gradient-to-t from-[#3CA2E2] bg-clip-text text-transparent "
             style={{
@@ -45,19 +54,19 @@ const Contact = () => {
             <div>
               <input
                 placeholder="Enter Your Name"
-                className="bg-[#ECEFF4] rounded-lg w-full p-2.5"
+                className="bg-[#ECEFF4] rounded-lg w-full p-2.5 focus:outline-2  outline-[#2178B5] "
               />
             </div>
             <div>
               <input
                 placeholder="Enter Your Mail"
-                className="bg-[#ECEFF4] rounded-lg w-full p-2.5"
+                className="bg-[#ECEFF4] rounded-lg w-full p-2.5 focus:outline-2 outline-[#2178B5]"
               />
             </div>
             <div>
               <textarea
                 placeholder="Enter Your Message"
-                className="bg-[#ECEFF4] rounded-lg w-full p-2.5 overflow-auto max-h-20 scroll-auto"
+                className="bg-[#ECEFF4] rounded-lg w-full p-2.5 overflow-auto h-24 max-h-full resize-none scroll-auto outline-[#2178B5]"
               />
             </div>
             <div className="flex justify-center ">
@@ -68,28 +77,32 @@ const Contact = () => {
           </form>
         </div>
         <div className="md:col-span-3 bg-gradient-to-r from-[#3CA2E2]  to-[#052EAA] rounded-lg grid grid-cols-2 grid-rows-1">
-          <div></div>
+          <div className="relative">
+            <Map/>
+          </div>
           <div className="p-6 ">
-            <div className="bg-[#F7F7F7] rounded-3xl">
+            <div className="bg-[#F7F7F7] rounded-3xl p-4">
               <Chart
                 chartType="PieChart"
                 data={data}
                 options={options}
                 width={"100%"}
-                height={"400px"}
+                height={"240px"}
               />
               <div className="flex justify-center items-center">
-                <div className="bg-white rounded-xl shadow-lg">
-                  <ul className="p-3  space-y-3">
-                    <li className="flex justify-center items-center">
-                      MSME{" "}
-                      <span className="bg-[#3CA2E2] h-6 w-6 inline-block rounded-md"></span>
+                <div className="bg-white rounded-xl shadow-lg ">
+                  <ul className="space-y-3 p-3">
+                    <li className="grid grid-cols-[auto_1.5rem] items-center gap-2 cursor-pointer ">
+                      <h2 className="paragraph">MSME</h2>
+                      <div className="bg-[#3CA2E2] h-6 w-6 rounded-md"></div>
                     </li>
-                    <li>
-                      STARTUP<span className="bg-[#052EAA]"></span>
+                    <li className="grid grid-cols-[auto_1.5rem] items-center gap-2 cursor-pointer">
+                      <h2 className="paragraph">STARTUP</h2>
+                      <div className="bg-[#052EAA] h-6 w-6 rounded-md"></div>
                     </li>
-                    <li>
-                      TAX <span className="bg-[#2D87C4]"></span>
+                    <li className="grid grid-cols-[auto_1.5rem] items-center gap-2 cursor-pointer">
+                      <h2 className="paragraph">TAX</h2>
+                      <div className="bg-[#2D87C4] h-6 w-6 rounded-md"></div>
                     </li>
                   </ul>
                 </div>
